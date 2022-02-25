7935b3685fb0e80df3c0ebadce51fde6593b4245
KatieFrogs <23621460+KatieFrogs@users.noreply.github.com>
2022-02-25 21:57:10 +0300

    Fix key events in the search box

--- public/src/js/pageevents.js
+++ public/src/js/pageevents.js
@@ -94,7 +94,9 @@ class PageEvents{
 		}
 		if(this.kbd.indexOf(event.key.toLowerCase()) !== -1){
 			this.lastKeyEvent = Date.now()
-			event.preventDefault()
+			if(event.target.tagName !== "INPUT"){
+				event.preventDefault()
+			}
 		}
 		this.keyListeners.forEach(addedKeyCode => {
 			this.checkListener(addedKeyCode.get("all"), event)
